6e1adc96c2965234b79c5a6dc209aff69ce435cb
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-19 01:58:25 -0700

    print("Assasslam o Alaikum. My name is Abdullah Amjad. I am doing bachelors from Paf-iast Fachhochschule Institute of Applied Sciences. Now I have clear the 4th semester. My SGPA is 3.2 and CGPA is 3.06. According to our university policy every 2 semesters Internship is must but if is it not in policy then I also want to do internship to learn more things and develop the skills. Now temporary these are my goals. ")
    print("These temporary goals i.e to learn more things are very important because first of all I will learn new things then I will see the going trend and change myself in that field and make my grip in that field more strong...")
    
    print(" First of all I will do different internships like currently I am doing 2 internships. One is in Artificial intelligence and second is in Python Development. So I am managing my time and takes the advantage of this free time because after that university is start then I dont get free time to learn extra things")
    
    print(" The Key challanges are everything that I am learning is mostly the new and i don't have any idea. So I am facing more challenges but problem solving is the good technique and skill to handle every challange and problem. To overcome these challanges make your base to strong that you know the each problem and if you understand the problem then it is easy to solve that...")
    
    print("Basially my goal is to learn more things and get more knowledge in different fields because i dont know the future trend that which field is going more popular. So if I have different knowledge then i will survive in future according to the trend")

---
+++ Day5R/task5testing_github.py
@@ -0,0 +1,10 @@
+print("Assasslam o Alaikum. My name is Abdullah Amjad. I am doing bachelors from Paf-iast Fachhochschule Institute of Applied Sciences. Now I have clear the 4th semester. My SGPA is 3.2 and CGPA is 3.06. According to our university policy every 2 semesters Internship is must but if is it not in policy then I also want to do internship to learn more things and develop the skills. Now temporary these are my goals. ")
+
+print("These temporary goals i.e to learn more things are very important because first of all I will learn new things then I will see the going trend and change myself in that field and make my grip in that field more strong...")
+
+print(" First of all I will do different internships like currently I am doing 2 internships. One is in Artificial intelligence and second is in Python Development. So I am managing my time and takes the advantage of this free time because after that university is start then I dont get free time to learn extra things")
+
+print(" The Key challanges are everything that I am learning is mostly the new and i don't have any idea. So I am facing more challenges but problem solving is the good technique and skill to handle every challange and problem. To overcome these challanges make your base to strong that you know the each problem and if you understand the problem then it is easy to solve that...")
+
+
+print("Basially my goal is to learn more things and get more knowledge in different fields because i dont know the future trend that which field is going more popular. So if I have different knowledge then i will survive in future according to the trend")
