8869b6d42dd858458e363ac4687cc4e3bd350e67
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-22 01:00:31 -0700

    # Taking array from the user array  = input("Enter a array : ")
    # reserving the array
    reverse_array = array[::-1]
    
    # Displaying the array in reverse order
    print("The reversed array  is :", reverse_array)

---
+++ Day6/task7.py
@@ -0,0 +1,8 @@
+# Taking array from the user
+array  = input("Enter a array : ")
+
+# reserving the array
+reverse_array = array[::-1]
+
+# Displaying the array in reverse order
+print("The reversed array  is :", reverse_array)
