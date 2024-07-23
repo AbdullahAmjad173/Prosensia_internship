cbb818ac75b76a68379f6d24b076d2960269ded9
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-22 22:46:10 -0700

    x = int(input("Enter the value to chack wheather it is less then 2 or grater then 2 : "))
    if x > 2:
        print('Bigger than 2')
        print('Still bigger')
    elif x < 2:
        print('Number is too small')
    else:
        print("The number is 2 not greater and not less then 2 ")
    
    print('The number you enter is ',x)

---
+++ Day7/task2.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the value to chack wheather it is less then 2 or grater then 2 : "))
+
+if x > 2:
+    print('Bigger than 2')
+    print('Still bigger')
+elif x < 2:
+    print('Number is too small')
+else:
+    print("The number is 2 not greater and not less then 2 ")
+    
+print('The number you enter is ',x)
