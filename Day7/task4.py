872ba2af1e406bd2bde7f698404148d14df121aa
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-22 23:03:55 -0700

    num = int(input(" Enter the number :: ")) #checking the condition if 0<= num <=20:     print("The number is in the range of 0 to 20 ")
    else:
        print("The number is not in the given range ")

---
+++ Day7/task4.py
@@ -0,0 +1,8 @@
+num = int(input(" Enter the number :: "))
+#checking the condition
+if 0<= num <=20:
+    print("The number is in the range of 0 to 20 ")
+    
+else:
+    print("The number is not in the given range ")
+    
