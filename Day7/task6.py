15774ef2fc432be6e9abf0cf2fe17b321af628b1
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-22 23:13:53 -0700

    x = int(input("Enter the value  : "))
    if 10<= x <= 20:
        print(' Number is medium in the range of 10 to 20 ')
    
    elif x < 10:
        print('Number is too small means less then 10 ')
    else:
        print("The number is larger means greater then 20 ")
    
    print('The number you enter is ',x)

---
+++ Day7/task6.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the value  : "))
+
+if 10<= x <= 20:
+    print(' Number is medium in the range of 10 to 20 ')
+    
+elif x < 10:
+    print('Number is too small means less then 10 ')
+else:
+    print("The number is larger means greater then 20 ")
+    
+print('The number you enter is ',x)
