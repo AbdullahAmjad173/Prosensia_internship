e9aa11241f0e33a2872cfdb1a9ffe8b9c90ef7c9
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-23 22:10:15 -0700

    #using function for conversion def convert_datatype(nmu):
        return float(num)
    
    #input from user
    num = int(input(" Enter a value : "))
    
    result = convert_datatype(num)
    
    #display the result
    print(result)

---
+++ Day8/task3.py
@@ -0,0 +1,12 @@
+#using function for conversion
+def convert_datatype(nmu):
+    
+    return float(num)
+
+#input from user
+num = int(input(" Enter a value : "))
+
+result = convert_datatype(num)
+
+#display the result
+print(result)
