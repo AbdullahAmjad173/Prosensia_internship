4dc49449adeb2a377b40d49722941e8bde1ab623
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-23 23:12:47 -0700

    #defining the function def cal_opr():     opr = 1 + 2 * float(3) / 4 - 5     return opr
        #display the function
        print("""The answer of this expression is   1 + 2 * float(3) / 4 - 5
              after evaluation is
              """)
    
    opr = cal_opr()
    print(opr)

---
+++ Day8/task10.py
@@ -0,0 +1,13 @@
+#defining the function
+def cal_opr():
+    opr = 1 + 2 * float(3) / 4 - 5
+    return opr
+    
+   
+    #display the function
+    print("""The answer of this expression is   1 + 2 * float(3) / 4 - 5   
+          after evaluation is 
+          """)
+    
+opr = cal_opr()
+print(opr)    
