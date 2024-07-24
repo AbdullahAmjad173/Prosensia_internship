93ac29c8da8bcba663d7478ccf21f6e2c47c79f2
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-23 22:14:19 -0700

    #DEfining the function for sum def add_numbers (num1, num2):     sum_total = num1 + num2     return sum_total
    #input from user
    num1 = int (input("Enter the first value"))
    num2 = int (input("Enter the second value"))
    
    sum_total = add_numbers(num1,num2)
    #Display the result
    print (sum_total)

---
+++ Day8/task4.py
@@ -0,0 +1,12 @@
+#DEfining the function for sum
+def add_numbers (num1, num2):
+    sum_total = num1 + num2 
+    return sum_total
+
+#input from user
+num1 = int (input("Enter the first value"))
+num2 = int (input("Enter the second value"))
+
+sum_total = add_numbers(num1,num2)
+#Display the result
+print (sum_total)
