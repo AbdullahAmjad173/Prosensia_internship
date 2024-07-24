36a5d2f8e2a7bc6d1c9ee8b2c5284ee6a6b3b669
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-23 22:41:25 -0700

    #usinf function to display the lyrics def print_lyrics():     lyrics = """     Twinkle, twinkle, little star,     How I wonder what you are!     Up above the world so high,     Like a diamond in the sky.
        Twinkle, twinkle, little star,
        How I wonder what you are!
        """
        print(lyrics)
    
    # Call the function to display the lyrics
    print_lyrics()

---
+++ Day8/task8.py
@@ -0,0 +1,15 @@
+#usinf function to display the lyrics
+def print_lyrics():
+    lyrics = """
+    Twinkle, twinkle, little star,
+    How I wonder what you are!
+    Up above the world so high,
+    Like a diamond in the sky.
+
+    Twinkle, twinkle, little star,
+    How I wonder what you are!
+    """
+    print(lyrics)
+
+# Call the function to display the lyrics
+print_lyrics()
