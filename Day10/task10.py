6921babdf5ed7a19c2ae53ef576e6c611c03ed2f
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-25 23:02:56 -0700

    import re
    def clean_string(input_string):
        # Remove leading and trailing whitespace
        trimmed_string = input_string.strip()
    
        # Replace any sequence of  whitespace with a single space
        cleaned_string = re.sub(r'\s+', ' ', trimmed_string)
    
        return cleaned_string
    
    original_string = "   This   is  a    string with   irregular  spacing.   "
    cleaned = clean_string(original_string)
    print(cleaned)  #

---
+++ Day10/task10.py
@@ -0,0 +1,15 @@
+import re
+
+def clean_string(input_string):
+    # Remove leading and trailing whitespace
+    trimmed_string = input_string.strip()
+    
+    # Replace any sequence of  whitespace with a single space
+    cleaned_string = re.sub(r'\s+', ' ', trimmed_string)
+    
+    return cleaned_string
+
+
+original_string = "   This   is  a    string with   irregular  spacing.   "
+cleaned = clean_string(original_string)
+print(cleaned)  #
