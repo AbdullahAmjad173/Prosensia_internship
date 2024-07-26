dba716e52a3c19664fde65d605c67b38228f2b5c
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-25 22:48:44 -0700

    def print_chars_with_index(string):     # Iterate through the string gives us both characher and index     for index, char in enumerate(string):         # Print the index and character
            print(index,char)
    
    print_chars_with_index("Hello, World!")

---
+++ Day10/task4.py
@@ -0,0 +1,9 @@
+def print_chars_with_index(string):
+    # Iterate through the string gives us both characher and index
+    for index, char in enumerate(string):
+        # Print the index and character
+    
+        print(index,char)
+
+
+print_chars_with_index("Hello, World!")
