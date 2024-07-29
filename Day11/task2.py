7655c1ce10f4320b930b40a4e292ab57b39e80be
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-28 23:10:46 -0700

    def convert_int_to_string(n):     return str(n)
    # sending strings:
    print(convert_int_to_string(123))  # Output: "123"
    print(convert_int_to_string(456))  # Output: "456"
    print(convert_int_to_string(789))  # Output: "789"

---
+++ Day11/task2.py
@@ -0,0 +1,7 @@
+def convert_int_to_string(n):
+    return str(n)
+
+# sending strings:
+print(convert_int_to_string(123))  # Output: "123"
+print(convert_int_to_string(456))  # Output: "456"
+print(convert_int_to_string(789))  # Output: "789"
