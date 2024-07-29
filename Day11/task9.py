10821eedef6256f5fceef1c683400d7f00b7c921
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-28 23:27:25 -0700

    def concatenate_two_strings():     str1 = input("Enter the first string: ")     str2 = input("Enter the second string: ")     return str1 + str2
    # Display the result :
    print(concatenate_two_strings())

---
+++ Day11/task9.py
@@ -0,0 +1,7 @@
+def concatenate_two_strings():
+    str1 = input("Enter the first string: ")
+    str2 = input("Enter the second string: ")
+    return str1 + str2
+
+# Display the result :
+print(concatenate_two_strings())
