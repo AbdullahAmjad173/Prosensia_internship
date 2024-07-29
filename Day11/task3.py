547649f42f95c6b91f0a8722396cc14fb89fc455
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-28 23:13:09 -0700

    def convert_float_to_int(f):     return int(f)
    # sending string values:
    print(convert_float_to_int(123.45))  # Output: 123
    print(convert_float_to_int(456.78))  # Output: 456
    print(convert_float_to_int(789.99))  # Output: 789
    print(convert_float_to_int(-123.45))  # Output: -124

---
+++ Day11/task3.py
@@ -0,0 +1,10 @@
+
+
+def convert_float_to_int(f):
+    return int(f)
+
+# sending string values:
+print(convert_float_to_int(123.45))  # Output: 123
+print(convert_float_to_int(456.78))  # Output: 456
+print(convert_float_to_int(789.99))  # Output: 789
+print(convert_float_to_int(-123.45))  # Output: -124
