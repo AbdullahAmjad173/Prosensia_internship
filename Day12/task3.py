f1a4fd8ec17c34196697f2eb210f88598497ae77
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-29 22:47:13 -0700

    import math
    def float_to_int_with_rounding(f, strategy):
        if strategy == "up":
            return math.ceil(f)
        elif strategy == "down":
            return math.floor(f)
        elif strategy == "nearest":
            return round(f)
        else:
            raise ValueError("Invalid rounding strategy")
    
    # calling function and display the result:
    print(float_to_int_with_rounding(3.7, "up"))      # Output: 4
    print(float_to_int_with_rounding(3.7, "down"))    # Output: 3
    print(float_to_int_with_rounding(3.7, "nearest")) # Output: 4

--- Day12/task3.py
+++ Day12/task3.py
@@ -1,1 +1,17 @@
 
+import math
+
+def float_to_int_with_rounding(f, strategy):
+    if strategy == "up":
+        return math.ceil(f)
+    elif strategy == "down":
+        return math.floor(f)
+    elif strategy == "nearest":
+        return round(f)
+    else:
+        raise ValueError("Invalid rounding strategy")
+
+# calling function and display the result:
+print(float_to_int_with_rounding(3.7, "up"))      # Output: 4
+print(float_to_int_with_rounding(3.7, "down"))    # Output: 3
+print(float_to_int_with_rounding(3.7, "nearest")) # Output: 4
