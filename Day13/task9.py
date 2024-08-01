38f96c3f3a06d9b99bbf1692839b5fa83fe3c54e
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-31 23:45:10 -0700

    def match_pattern(s, pattern):     if len(s) != len(pattern):         return False
        for char_s, char_pattern in zip(s, pattern):
            if char_pattern.isalpha() and not char_s.isalpha():
                return False
            if char_pattern.isdigit() and not char_s.isdigit():
                return False
    
        return True
    
    # Passing Values
    s = "a1b2"
    pattern = "a1b2"
    result = match_pattern(s, pattern)
    print(f"Pattern matches: {result}")

--- Day13/task9.py
+++ Day13/task9.py
@@ -1,1 +1,17 @@
+def match_pattern(s, pattern):
+    if len(s) != len(pattern):
+        return False
+    
+    for char_s, char_pattern in zip(s, pattern):
+        if char_pattern.isalpha() and not char_s.isalpha():
+            return False
+        if char_pattern.isdigit() and not char_s.isdigit():
+            return False
+    
+    return True
 
+# Passing Values
+s = "a1b2"
+pattern = "a1b2"
+result = match_pattern(s, pattern)
+print(f"Pattern matches: {result}")
