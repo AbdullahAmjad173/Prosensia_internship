227e5e35b9c9e1e98d7c7d72b66a7244b9217180
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-07-31 23:29:25 -0700

    def binary_conversion(binary_str, fixed_length):     # Convert binary string to integer     integer = int(binary_str, 2)
        binary_str_back = bin(integer)[2:]  # [2:] to remove the '0b' prefix
    
        binary_str_back = binary_str_back.zfill(fixed_length)
    
        return binary_str_back
    
    #passing values to function
    binary_str = "101"
    fixed_length = 8
    result = binary_conversion(binary_str, fixed_length)
    print(result)

--- Day13/task2.py
+++ Day13/task2.py
@@ -1,1 +1,16 @@
 
+def binary_conversion(binary_str, fixed_length):
+    # Convert binary string to integer
+    integer = int(binary_str, 2)
+    
+    binary_str_back = bin(integer)[2:]  # [2:] to remove the '0b' prefix
+
+    binary_str_back = binary_str_back.zfill(fixed_length)
+    
+    return binary_str_back
+
+#passing values to function
+binary_str = "101"
+fixed_length = 8
+result = binary_conversion(binary_str, fixed_length)
+print(result)
