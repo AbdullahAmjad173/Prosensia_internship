d3b7ba12028d4b492bf37ed829984ffcda48ecf2
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-01 23:38:10 -0700

    import scipy.integrate as spi
    def adaptive_integration(func, a, b, tol=1e-6):
        result, _ = spi.quad(func, a, b, epsabs=tol)
        return result
    
    # passing values to the function.
    print(adaptive_integration(lambda x: x**2, 0, 1))
    print(adaptive_integration(lambda x: x**3, 0, 1))

--- Day15/task4.py
+++ Day15/task4.py
@@ -4,6 +4,6 @@ def adaptive_integration(func, a, b, tol=1e-6):
     result, _ = spi.quad(func, a, b, epsabs=tol)
     return result
 
-# passing values to the function
+# passing values to the function.
 print(adaptive_integration(lambda x: x**2, 0, 1))
 print(adaptive_integration(lambda x: x**3, 0, 1))
