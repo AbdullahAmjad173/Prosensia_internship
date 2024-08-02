7d2da0617f6f1b02a050fcb844453f52f83ca808
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-01 23:39:49 -0700

    import re
    def extract_dates(input_string):
        date_pattern = r'\b\d{4}[-/]\d{2}[-/]\d{2}\b'
        dates = re.findall(date_pattern, input_string)
        return dates
    
    # passing values to the function.
    print(extract_dates("The event is on 2023-08-15 and the deadline was 2023-07-31."))

--- Day15/task7.py
+++ Day15/task7.py
@@ -5,5 +5,5 @@ def extract_dates(input_string):
     dates = re.findall(date_pattern, input_string)
     return dates
 
-# passing values to the function
+# passing values to the function.
 print(extract_dates("The event is on 2023-08-15 and the deadline was 2023-07-31."))
