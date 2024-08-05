36b3bdbf6c52209fa86b079d3831732447e85d00
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-05 00:06:55 -0700

    # Count the number of lines in data.txt with open('data.txt', 'r') as file:     lines = file.readlines()     line_count = len(lines)     print(f"Number of lines in data.txt: {line_count}")

---
+++ Day16/task5.py
@@ -0,0 +1,5 @@
+# Count the number of lines in data.txt
+with open('data.txt', 'r') as file:
+    lines = file.readlines()
+    line_count = len(lines)
+    print(f"Number of lines in data.txt: {line_count}")
