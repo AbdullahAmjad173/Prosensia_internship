4f99a9db5b769682cee80f2c61ed0fe4eedc521c
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-05 00:08:14 -0700

    # Find and print all lines that contain the word "error" in log.txt try:     with open('log.txt', 'r') as file:         lines = file.readlines()     for line in lines:         if 'error' in line:             print(line, end='') except FileNotFoundError:     print("The file data.txt does not exist.")

---
+++ Day16/task6.py
@@ -0,0 +1,9 @@
+# Find and print all lines that contain the word "error" in log.txt
+try:
+    with open('log.txt', 'r') as file:
+        lines = file.readlines()
+    for line in lines:
+        if 'error' in line:
+            print(line, end='')
+except FileNotFoundError:
+    print("The file data.txt does not exist.")
