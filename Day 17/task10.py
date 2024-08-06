b52b40d163942d17c875371b17b617624da66fac
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-05 22:36:25 -0700

    # Given lists list1 = [1, 2, 3] list2 = [4, 5, 6] # Concatenating the lists concatenated_list = list1 + list2 # Printing the concatenated list print(concatenated_list)

---
+++ Day 17/task10.py
@@ -0,0 +1,6 @@
+# Given list
+numbers = [1, 2, 3, 4, 5]
+# Creating a new list with the squares of all elements
+squares = [num ** 2 for num in numbers]
+# Printing the new list
+print(squares)
---
+++ Day 17/task9.py
@@ -0,0 +1,7 @@
+# Given lists
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+# Concatenating the lists
+concatenated_list = list1 + list2
+# Printing the concatenated list
+print(concatenated_list)
