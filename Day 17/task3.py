aab21b4e4f0426e582cb30f51b7d049d9a423cbf
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-05 22:31:31 -0700

    # Given list colors = ['red', 'green', 'blue', 'yellow', 'purple'] # Slicing to get the first three elements sliced_colors = colors[:3] # Printing the sliced list print(sliced_colors)

--- Day 17/task3.py
+++ Day 17/task3.py
@@ -1,1 +1,6 @@
-as
+# Given list
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+# Slicing to get the first three elements
+sliced_colors = colors[:3]
+# Printing the sliced list
+print(sliced_colors)
