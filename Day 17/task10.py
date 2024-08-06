9086b09fea1157d33512df3d548d64394a007e23
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-05 22:37:33 -0700

    # Given list numbers = [1, 2, 3, 4, 5] # Creating a new list with the squares of all elements squares = [num ** 2 for num in numbers] # Printing the new list. print(squares)

--- Day 17/task10.py
+++ Day 17/task10.py
@@ -2,5 +2,5 @@
 numbers = [1, 2, 3, 4, 5]
 # Creating a new list with the squares of all elements
 squares = [num ** 2 for num in numbers]
-# Printing the new list
+# Printing the new list.
 print(squares)
