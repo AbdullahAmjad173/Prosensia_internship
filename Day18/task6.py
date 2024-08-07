de8481b83b68e0cf2cbaf84af203c9e6219df219
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-07 00:16:05 -0700

    # Using the get method to retrieve values with a default of 0. my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'} key = 'key4' value = my_dict.get(key, 0) print("The value for '{}' is: {}".format(key, value))

--- Day18/task6.py
+++ Day18/task6.py
@@ -1,4 +1,4 @@
-# Using the get method to retrieve values with a default of 0
+# Using the get method to retrieve values with a default of 0.
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 key = 'key4'
 value = my_dict.get(key, 0)
