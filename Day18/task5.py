e91e79f42b3400bc22f92f8b3022c5e2d52f6526
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-07 00:15:42 -0700

    def print_keys_values(dictionary):     keys = list(dictionary.keys())     values = list(dictionary.values())     print("Keys:", keys)     print("Values:", values)
    # Printing the values
    my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
    print_keys_values(my_dict)

--- Day18/task5.py
+++ Day18/task5.py
@@ -4,6 +4,6 @@ def print_keys_values(dictionary):
     print("Keys:", keys)
     print("Values:", values)
 
-# Example usage
+# Printing the values
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 print_keys_values(my_dict)
