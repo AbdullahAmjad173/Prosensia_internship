279b4740b06bc82b47b41107dde30130af7aac97
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-07 00:13:46 -0700

    def print_value(dictionary, key):     if key in dictionary:         print("Value:", dictionary[key])     else:         print("The key '{}' is not found in the dictionary.".format(key))
    # printing the values
    my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
    print_value(my_dict, 'key2')
    print_value(my_dict, 'key4')

--- Day18/task2.py
+++ Day18/task2.py
@@ -4,7 +4,7 @@ def print_value(dictionary, key):
     else:
         print("The key '{}' is not found in the dictionary.".format(key))
 
-# Example usage
+# printing the values
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 print_value(my_dict, 'key2')
 print_value(my_dict, 'key4')
