fca42b5aff356c7da72abdba207be845b6df54ab
AbdullahAmjad173 <abdullahamjad17301@gmail.com>
2024-08-07 22:59:47 -0700

    # Create a tuple with three numerical values numbers = (4, 7, 1)
    # Function to return the maximum value in the tuple
    def max_in_tuple(tup):
        return max(tup)
    
    # printing the result
    print("Maximum value:", max_in_tuple(numbers))

---
+++ Day19/task2.py
@@ -0,0 +1,9 @@
+# Create a tuple with three numerical values
+numbers = (4, 7, 1)
+
+# Function to return the maximum value in the tuple
+def max_in_tuple(tup):
+    return max(tup)
+
+# printing the result
+print("Maximum value:", max_in_tuple(numbers))
